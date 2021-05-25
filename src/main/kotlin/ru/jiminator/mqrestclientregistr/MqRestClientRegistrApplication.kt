e41334c411329b7a65dfb69afbd10f687bc704af
Jiminator3 <timyrshow72@gmail.com>
2021-05-25 19:39:43 +0300

    Fixed spelling errors in comments

--- src/main/kotlin/ru/jiminator/mqrestclientregistr/MqRestClientRegistrApplication.kt
+++ src/main/kotlin/ru/jiminator/mqrestclientregistr/MqRestClientRegistrApplication.kt
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 import javax.persistence.*
 
 
@@ -40,7 +39,7 @@ class ClientConsumer(private val clients: ClientRepository) {
     @KafkaListener(topics = ["external.in.client-info"])
     fun clientSave(message: Client) {
         logger.info { "Get message: $message" }
-        // Сохраняем в СУБД.
+        // Сохраняем в БД.
         clients.save(message)
     }
 
