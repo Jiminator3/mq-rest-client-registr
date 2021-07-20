7feff20b24edad69c585448dba4b6e0145d269b1
Timur Pospelov <tpospelov@citc.ru>
2021-07-20 18:21:42 +0300

    Work from WebMvc.fn route DSL

--- src/main/kotlin/ru/jiminator/mqrestclientregistr/MqRestClientRegistrApplication.kt
+++ src/main/kotlin/ru/jiminator/mqrestclientregistr/MqRestClientRegistrApplication.kt
@@ -6,12 +6,21 @@ import au.com.console.kassava.kotlinToString
 import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.repository.CrudRepository
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.function.HandlerFunction
+import org.springframework.web.servlet.function.RouterFunction
+import org.springframework.web.servlet.function.RouterFunctions.route
+import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.ServerResponse.*
+import org.springframework.web.servlet.function.router
+import java.util.*
 import javax.persistence.*
 
 
@@ -24,8 +33,36 @@ fun main(args: Array<String>) {
 
 interface ClientRepository : CrudRepository<Client, Long>
 
+interface BusinessData {
+    sealed interface Type {
+        val name: String
+    }
+
+    companion object {
+        // TODO some real impl
+        fun parseType(typeName: String): Type = BusinessDataType(typeName)
+    }
+}
+
+//@Serializable(with = DocumentIdSerializer::class)
+@JvmInline
+value class DocumentId(override val uuid: UUID) : UuidBasedId<Client>
+
+interface UuidBasedId<T> : Id<T> {
+    val uuid: UUID
+    override fun encodeAsString() = uuid.toString()
+}
+
+@JvmInline
+value class BusinessDataType(override val name: String) : BusinessData.Type
+
 @Entity
-class Client(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null, val factAddress: String, val regAddress: String, val phone: Long) {
+class Client(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    val factAddress: String,
+    val regAddress: String,
+    val phone: Long
+) {
 
     companion object {
         private val properties = arrayOf(Client::factAddress, Client::regAddress, Client::phone)
@@ -38,33 +75,64 @@ class Client(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Lon
     override fun hashCode() = kotlinHashCode(properties = properties)
 }
 
-// Получаем с Kafka JSON сообщение.
-@Component
-class ClientConsumer(private val clients: ClientRepository) {
+@Service
+class ClientService(private val clients: ClientRepository) {
 
-    private val logger = KotlinLogging.logger {}
-
-    @KafkaListener(topics = ["external.in.client-info"])
-    fun clientSave(message: Client) {
-        logger.info { "Get message: $message" }
-        // Сохраняем в БД.
-        clients.save(message)
-    }
-
-}
-
-// REST API позволяет получить данные по этим объектам.
-@RestController
-class ClientResource(private val clients: ClientRepository) {
-
-    @GetMapping("/client")
-    fun index(): List<Client> {
+    fun all(): List<Client> {
         return clients.findAll().toList()
     }
 
-    @GetMapping("/client/{id}")
-    fun findClientById(@PathVariable id: Long): Client? {
+    fun byId(id: Long): Client? {
         return clients.findById(id).orElse(null)
-
     }
 }
+
+@Bean
+fun routes(clients: ClientService): RouterFunction<ServerResponse> {
+    return route()
+        .GET("/client", serverRequest -> ServerResponse.ok().body( clients.all() ))
+}
+
+
+internal fun buildLegacyApiRoutes(restApiHandler: RestApiHandler) = router {
+    "/".nest {
+        GET("/client/{id}") { request ->
+            val id = runCatching { request.pathVariable("id") }
+            if (id.isSuccess) {
+                restApiHandler.Clients(id.getOrThrow())
+            } else {
+                ServerResponse.badRequest().body(id.exceptionOrNull()?.message.orEmpty())
+            }
+        }
+    }
+
+    // Получаем с Kafka JSON сообщение.
+    @Component
+    class ClientConsumer(private val clients: ClientRepository) {
+
+        private val logger = KotlinLogging.logger {}
+
+        @KafkaListener(topics = ["external.in.client-info"])
+        fun clientSave(message: Client) {
+            logger.info { "Get message: $message" }
+            // Сохраняем в БД.
+            clients.save(message)
+        }
+
+    }
+
+    // REST API позволяет получить данные по этим объектам.
+    @RestController
+    class ClientResource(private val clients: ClientRepository) {
+
+        @GetMapping("/client")
+        fun index(): List<Client> {
+            return clients.findAll().toList()
+        }
+
+        @GetMapping("/client/{id}")
+        fun findClientById(@PathVariable id: Long): Client? {
+            return clients.findById(id).orElse(null)
+
+        }
+    }
---
+++ src/main/kotlin/ru/jiminator/mqrestclientregistr/RestApiHandler.kt
@@ -0,0 +1,60 @@
+package ru.jiminator.mqrestclientregistr
+
+import org.apache.logging.log4j.kotlin.logger
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.ServerResponse.notFound
+import org.springframework.web.servlet.function.ServerResponse.ok
+import org.springframework.web.servlet.function.ServerResponse.status
+import java.net.HttpURLConnection.HTTP_NOT_FOUND
+import java.net.HttpURLConnection.HTTP_OK
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+
+class RestApiHandler(
+    private val repository: ClientRepository,
+    private val documentDownloadUrl: URI,
+) {
+
+    fun Clients(id: String): ServerResponse {
+        val documentExtId: List<Client> = repository.findAll().toList() ?: return notFound().build()
+        val request = HttpRequest.newBuilder()
+            .uri(documentDownloadUrl.resolve(documentExtId))
+            .build()
+        val resp = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream())
+        return when (resp.statusCode()) {
+            HTTP_OK -> {
+                val contentLen = resp.headers().firstValue("Content-Length").map { it.toLong() }.orElse(null)
+                val mediaType = resp.headers().firstValue("Content-Type")
+                    .map { MediaType.parseMediaType(it) }
+                    .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                val stream = resp.body()
+                ok().apply {
+                    contentType(mediaType)
+                    if (contentLen != null) {
+                        contentLength(contentLen)
+                    }
+                }.body(InputStreamResource(stream))
+            }
+            HTTP_NOT_FOUND -> {
+                notFound().build()
+            }
+            else -> {
+                log.warn { "Unprocessable response status $resp for $documentId." }
+                status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+            }
+        }
+    }
+}
+
+//fun Clients(id: String): ServerResponse {
+//        return clients.findAll().toList()
+//    }
+//
+//}
